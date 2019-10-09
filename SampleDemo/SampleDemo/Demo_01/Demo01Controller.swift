1864f822914c750e2c84ee24cb10266a8f8cfec8
lishen <shen_li@dingyuegroup.cn>
2019-10-10 00:34:01 +0800

    collection 头部放大效果

--- SampleDemo/SampleDemo/Demo_01/Demo01Controller.swift
+++ SampleDemo/SampleDemo/Demo_01/Demo01Controller.swift
@@ -12,21 +12,95 @@ class Demo01Controller: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
     
     override func initView() {
         super.initView()
+        
+        
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  
+    lazy var collectionView:UICollectionView = {
+        let layout = Demo01FlowLayout()
+        layout.itemSize = CGSize(width: view.qmui_width, height: 40)
+        layout.headerReferenceSize = CGSize(width: view.qmui_width, height: 200)
+        let coll = UICollectionView.init(frame: .zero, collectionViewLayout: layout)
+        coll.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        coll.register(Demo01HeadView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "Demo01HeadView")
+        coll.backgroundColor = .white
+        coll.dataSource = self
+        coll.delegate = self
+        return coll
+    }()
+}
+extension Demo01Controller:UICollectionViewDataSource,UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.qmui_random()
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Demo01HeadView", for: indexPath)
+            return header
+        }
+        return UICollectionReusableView.init()
+    }
+    
+}
+class Demo01FlowLayout:UICollectionViewFlowLayout {
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let offset : CGPoint = collectionView?.contentOffset ?? .zero
+        let attrs : [UICollectionViewLayoutAttributes] = super.layoutAttributesForElements(in: rect) ?? []
+        if offset.y < 0 {
+            let deltaY  = CGFloat(fabsf(Float(offset.y)))
+            
+            for item in attrs {
+                if let kind = item.representedElementKind,kind == UICollectionView.elementKindSectionHeader {
+                    let headerSize = headerReferenceSize
+                    var headRect = item.frame
+                    headRect.size.width = headerSize.width + deltaY
+                    headRect.size.height = headerSize.height + deltaY
+                    headRect.origin.x = headRect.origin.x - deltaY/2
+                    headRect.origin.y = headRect.origin.y - deltaY
+                    item.frame = headRect
+                    break
+                }
+                
+            }
+        }
+        
+        return attrs
+    }
+}
+class Demo01HeadView: UICollectionReusableView {
+    let image = UIImageView()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.qmui_random()
+        
+        image.image = UIImage(named: "icon_read_taskad_day")
+        image.backgroundColor = .red
+        addSubview(image)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        image.frame = bounds
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- SampleDemo/SampleDemo/LeftController.swift
+++ SampleDemo/SampleDemo/LeftController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import QMUIKit
 
 class LeftController: BaseViewController {
     
-    let data:[[String:Any]] = [
-        
+    let datas:[[String:Any]] = [
+        ["下拉头部变大":Demo01Controller()],
+         ["1111":Demo01Controller()],
+         ["22222":Demo01Controller()]
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +22,41 @@ class LeftController: BaseViewController {
     }
     lazy var tableView:UITableView = {
         let tv = UITableView.init(frame: .zero, style: .plain)
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tv.dataSource = self
+        tv.delegate = self
         return tv
     }()
     override func initView() {
+        tableView.frame = CGRect(x: 0, y:44 , width: view.qmui_width, height: view.qmui_height-44)
+        view.addSubview(tableView)
+        
+    }
+}
+extension LeftController:UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let data = datas[indexPath.row]
+        cell.textLabel?.text = data.keys.first
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //let data = datas[indexPath.row]
+        var desVc:UIViewController?
+        switch indexPath.row {
+        case 0:
+            desVc = Demo01Controller()
+        default:
+            break
+        }
+        if let vc = desVc , let resVc = self.revealViewController(){
+            resVc.pushFrontViewController(vc, animated: true)
+        }
         
     }
 }
